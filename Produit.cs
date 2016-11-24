c53bd545d69b328650e2d97ec5b3de34a2268556
Vincent DUPONT <0xVinC@gmail.com>
2016-11-24 16:51:40 +0100

    Creation de la classe Produit
    Création de la classe Produit

---
+++ Produit.cs
@@ -0,0 +1,30 @@
+
+class Produit
+    {
+        int num;
+        String nom;
+        String effet;
+        String contreIndication;
+        String presentation;
+        String dosage;
+        String famille;
+        String interaction;
+        double prixHT;
+        double prixEchantillon;
+
+        public Produit(int unNum, string unNom, string unEffet, string desCI, string unePresentation, string unDosage, int unIDFamille, double unPrixHT, double unPrixEch, string uneInteraction)
+        {
+            num = unNum;
+            nom = unNom;
+            effet = unEffet;
+            contreIndication = desCI;
+            presentation = unePresentation;
+            dosage = unDosage;
+            famille = ;
+            prixHT = unPrixHT;
+            prixEchantillon = unPrixEch;
+            interaction=uneInteraction;
+        }
+
+    }
+       
