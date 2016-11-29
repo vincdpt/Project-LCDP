0717b282939c7a9373eafcd219256d8cbe59a455
JbEnLeuleu <jean-baptiste.alcaraz@icof.fr>
2016-11-29 16:20:30 +0100

    BDD

---
+++ BDD/backoffice_gsb.sql
@@ -0,0 +1,277 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Mar 29 Novembre 2016 à 15:19
+-- Version du serveur :  5.7.11
+-- Version de PHP :  5.6.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `backoffice_gsb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `famille`
+--
+
+CREATE TABLE `famille` (
+  `id_famille` int(5) NOT NULL,
+  `famille_produit` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `gestionnaire`
+--
+
+CREATE TABLE `gestionnaire` (
+  `id_gestionnaire` int(5) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `mdp` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `gestionnaire`
+--
+
+INSERT INTO `gestionnaire` (`id_gestionnaire`, `login`, `mdp`) VALUES
+(1, 'jean-baptiste', 'alcaraz'),
+(2, 'thibaud', 'demorand'),
+(3, 'vincent', 'dupont'),
+(4, 'kevin', 'vinke'),
+(5, 'alexandre', 'andrealex');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `praticien`
+--
+
+CREATE TABLE `praticien` (
+  `id_praticien` int(10) NOT NULL,
+  `id_specialite` int(5) NOT NULL,
+  `raison_sociale` varchar(20) NOT NULL,
+  `adresse` varchar(50) NOT NULL,
+  `telephone` varchar(15) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  `mail` varchar(40) NOT NULL,
+  `coeff_notoriete` double NOT NULL,
+  `coeff_confiance` double NOT NULL,
+  `type` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `produits`
+--
+
+CREATE TABLE `produits` (
+  `id_produit` int(5) NOT NULL,
+  `id_famille` int(5) NOT NULL,
+  `nom` varchar(25) NOT NULL,
+  `effets` varchar(50) NOT NULL,
+  `contre_indication` varchar(50) NOT NULL,
+  `interactions_autres_produits` varchar(50) NOT NULL,
+  `presentation` varchar(100) NOT NULL,
+  `dosage` double NOT NULL,
+  `prix_HT` double NOT NULL,
+  `prix_Echantillon` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `secteur`
+--
+
+CREATE TABLE `secteur` (
+  `id_secteur` int(5) NOT NULL,
+  `secteur_intervention` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `specialite`
+--
+
+CREATE TABLE `specialite` (
+  `id_specialite` int(5) NOT NULL,
+  `specialite_praticien` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `visiteur`
+--
+
+CREATE TABLE `visiteur` (
+  `id_visiteur` char(4) NOT NULL,
+  `id_secteur` int(5) DEFAULT NULL,
+  `nom` char(30) DEFAULT NULL,
+  `prenom` char(30) DEFAULT NULL,
+  `login` char(20) DEFAULT NULL,
+  `mdp` char(20) DEFAULT NULL,
+  `adresse` char(30) DEFAULT NULL,
+  `cp` char(5) DEFAULT NULL,
+  `ville` char(30) DEFAULT NULL,
+  `dateEmbauche` date DEFAULT NULL,
+  `Privileges` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `visiteur`
+--
+
+INSERT INTO `visiteur` (`id_visiteur`, `id_secteur`, `nom`, `prenom`, `login`, `mdp`, `adresse`, `cp`, `ville`, `dateEmbauche`, `Privileges`) VALUES
+('a131', NULL, 'Villechalane', 'Louis', 'lvillachane', 'jux7g', '8 rue des Charmes', '46000', 'Cahors', '2005-12-21', 0),
+('a17', NULL, 'Andre', 'David', 'dandre', 'oppg5', '1 rue Petit', '46200', 'Lalbenque', '1998-11-23', 1),
+('a55', NULL, 'Bedos', 'Christian', 'cbedos', 'gmhxd', '1 rue Peranud', '46250', 'Montcuq', '1995-01-12', 0),
+('a93', NULL, 'Tusseau', 'Louis', 'ltusseau', 'ktp3s', '22 rue des Ternes', '46123', 'Gramat', '2000-05-01', 0),
+('b13', NULL, 'Bentot', 'Pascal', 'pbentot', 'doyw1', '11 allée des Cerises', '46512', 'Bessines', '1992-07-09', 0),
+('b16', NULL, 'Bioret', 'Luc', 'lbioret', 'hrjfs', '1 Avenue gambetta', '46000', 'Cahors', '1998-05-11', 0),
+('b19', NULL, 'Bunisset', 'Francis', 'fbunisset', '4vbnd', '10 rue des Perles', '93100', 'Montreuil', '1987-10-21', 0),
+('b25', NULL, 'Bunisset', 'Denise', 'dbunisset', 's1y1r', '23 rue Manin', '75019', 'paris', '2010-12-05', 0),
+('b28', NULL, 'Cacheux', 'Bernard', 'bcacheux', 'uf7r3', '114 rue Blanche', '75017', 'Paris', '2009-11-12', 0),
+('b34', NULL, 'Cadic', 'Eric', 'ecadic', '6u8dc', '123 avenue de la République', '75011', 'Paris', '2008-09-23', 0),
+('b4', NULL, 'Charoze', 'Catherine', 'ccharoze', 'u817o', '100 rue Petit', '75019', 'Paris', '2005-11-12', 0),
+('b50', NULL, 'Clepkens', 'Christophe', 'cclepkens', 'bw1us', '12 allée des Anges', '93230', 'Romainville', '2003-08-11', 0),
+('b59', NULL, 'Cottin', 'Vincenne', 'vcottin', '2hoh9', '36 rue Des Roches', '93100', 'Monteuil', '2001-11-18', 0),
+('c14', NULL, 'Daburon', 'François', 'fdaburon', '7oqpv', '13 rue de Chanzy', '94000', 'Créteil', '2002-02-11', 0),
+('c3', NULL, 'De', 'Philippe', 'pde', 'gk9kx', '13 rue Barthes', '94000', 'Créteil', '2010-12-14', 0),
+('c54', NULL, 'Debelle', 'Michel', 'mdebelle', 'od5rt', '181 avenue Barbusse', '93210', 'Rosny', '2006-11-23', 0),
+('d13', NULL, 'Debelle', 'Jeanne', 'jdebelle', 'nvwqq', '134 allée des Joncs', '44000', 'Nantes', '2000-05-11', 0),
+('d51', NULL, 'Debroise', 'Michel', 'mdebroise', 'sghkb', '2 Bld Jourdain', '44000', 'Nantes', '2001-04-17', 0),
+('e22', NULL, 'Desmarquest', 'Nathalie', 'ndesmarquest', 'f1fob', '14 Place d Arc', '45000', 'Orléans', '2005-11-12', 0),
+('e24', NULL, 'Desnost', 'Pierre', 'pdesnost', '4k2o5', '16 avenue des Cèdres', '23200', 'Guéret', '2001-02-05', 0),
+('e39', NULL, 'Dudouit', 'Frédéric', 'fdudouit', '44im8', '18 rue de l église', '23120', 'GrandBourg', '2000-08-01', 0),
+('e49', NULL, 'Duncombe', 'Claude', 'cduncombe', 'qf77j', '19 rue de la tour', '23100', 'La souteraine', '1987-10-10', 0),
+('e5', NULL, 'Enault-Pascreau', 'Céline', 'cenault', 'y2qdu', '25 place de la gare', '23200', 'Gueret', '1995-09-01', 0),
+('e52', NULL, 'Eynde', 'Valérie', 'veynde', 'i7sn3', '3 Grand Place', '13015', 'Marseille', '1999-11-01', 0),
+('f21', NULL, 'Finck', 'Jacques', 'jfinck', 'mpb3t', '10 avenue du Prado', '13002', 'Marseille', '2001-11-10', 0),
+('f39', NULL, 'Frémont', 'Fernande', 'ffremont', 'xs5tq', '4 route de la mer', '13012', 'Allauh', '1998-10-01', 0),
+('f4', NULL, 'Gest', 'Alain', 'agest', 'dywvt', '30 avenue de la mer', '13025', 'Berre', '1985-11-01', 0);
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `famille`
+--
+ALTER TABLE `famille`
+  ADD PRIMARY KEY (`id_famille`);
+
+--
+-- Index pour la table `gestionnaire`
+--
+ALTER TABLE `gestionnaire`
+  ADD PRIMARY KEY (`id_gestionnaire`);
+
+--
+-- Index pour la table `praticien`
+--
+ALTER TABLE `praticien`
+  ADD PRIMARY KEY (`id_praticien`),
+  ADD KEY `praticien` (`id_specialite`);
+
+--
+-- Index pour la table `produits`
+--
+ALTER TABLE `produits`
+  ADD PRIMARY KEY (`id_produit`),
+  ADD KEY `produits` (`id_famille`);
+
+--
+-- Index pour la table `secteur`
+--
+ALTER TABLE `secteur`
+  ADD PRIMARY KEY (`id_secteur`);
+
+--
+-- Index pour la table `specialite`
+--
+ALTER TABLE `specialite`
+  ADD PRIMARY KEY (`id_specialite`);
+
+--
+-- Index pour la table `visiteur`
+--
+ALTER TABLE `visiteur`
+  ADD PRIMARY KEY (`id_visiteur`),
+  ADD KEY `visiteur` (`id_secteur`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `famille`
+--
+ALTER TABLE `famille`
+  MODIFY `id_famille` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `gestionnaire`
+--
+ALTER TABLE `gestionnaire`
+  MODIFY `id_gestionnaire` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT pour la table `praticien`
+--
+ALTER TABLE `praticien`
+  MODIFY `id_praticien` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `produits`
+--
+ALTER TABLE `produits`
+  MODIFY `id_produit` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `secteur`
+--
+ALTER TABLE `secteur`
+  MODIFY `id_secteur` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `specialite`
+--
+ALTER TABLE `specialite`
+  MODIFY `id_specialite` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `praticien`
+--
+ALTER TABLE `praticien`
+  ADD CONSTRAINT `praticien_ibfk_1` FOREIGN KEY (`id_specialite`) REFERENCES `specialite` (`id_specialite`);
+
+--
+-- Contraintes pour la table `produits`
+--
+ALTER TABLE `produits`
+  ADD CONSTRAINT `produits_ibfk_1` FOREIGN KEY (`id_famille`) REFERENCES `famille` (`id_famille`);
+
+--
+-- Contraintes pour la table `visiteur`
+--
+ALTER TABLE `visiteur`
+  ADD CONSTRAINT `visiteur_ibfk_1` FOREIGN KEY (`id_secteur`) REFERENCES `secteur` (`id_secteur`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
