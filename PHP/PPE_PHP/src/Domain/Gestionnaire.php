c630e88199c239e9fce2b21807f51600dbd64d01
JbEnLeuleu <jean-baptiste.alcaraz@icof.fr>
2016-12-06 11:58:27 +0100

    Finition famille.php et gestionnaire.php

---
+++ PHP/PPE_PHP/src/Domain/Gestionnaire.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace PPE_PHP\Domain;
+
+class Gestionnaire
+{
+    /**
+     * Gestionnaire id_gestionnaire.
+     *
+     * @var integer
+     */
+    private $id_gestionnaire;
+
+    /**
+     * Gestionnaire login.
+     *
+     * @var integer
+     */
+    private $login;
+
+    /**
+     * Gestionnaire mdp.
+     *
+     * @var integer
+     */
+    private $mdp;
+
+
+    // id_gestionnaire
+    public function getIdGestionnaire() {
+        return $this->id_gestionnaire;
+    }
+
+    public function setIdGestionnaire($id_gestionnaire) {
+        $this->id_gestionnaire = $id_gestionnaire;
+    }
+
+    // login
+    public function getLoginGestionnaire() {
+        return $this->login;
+    }
+
+    public function setLoginGestionnaire($login) {
+        $this->login = $login;
+    }
+
+    // mdp
+    public function getMdpGestionnaire() {
+        return $this->mdp;
+    }
+
+    public function SetMdpGestionnaire($mdp) {
+        $this->mdp = $mdp;
+    }
+
+
+}
